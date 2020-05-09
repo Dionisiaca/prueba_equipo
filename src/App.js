461e4783d39d682e416c95501052c8e92286ea82
26erika <develop.erika@gmail.com>
2020-05-09 12:52:55 -0500

    merge test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola mundo!</h1>
+      <p>dice claudia</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/try.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const sdsads = (sd) => {
+    return ( dsds );
+}
+ 
+export default sdsads;
+class  extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ;
